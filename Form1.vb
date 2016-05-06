c9258227c320a47221296781af29eaefd20b0a77
jwmueller85 <jwmueller6985@gmail.com>
2016-05-05 23:54:51 -0500

    Added files via upload
    Work in progress. Creating an application in VB that can view and make changes to a SQL database.

---
+++ Form1.vb
@@ -0,0 +1,56 @@
+﻿Public Class Form1
+    Dim SQL As New SQLControl
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnQuery_Click(sender As Object, e As EventArgs) Handles btnQuery.Click
+        If txtQuery.Text <> "" Then 'keeps it from crashing if button pressed and no text in box
+            If SQL.HasConnection = True Then 'checks connection
+                SQL.RunQuery(txtQuery.Text)
+
+                If SQL.SQLDataset.Tables.Count > 0 Then 'makes sure data is in table 
+                    dgvData.DataSource = SQL.SQLDataset.Tables(0)
+
+                End If
+            End If
+        End If
+
+    End Sub
+
+    Private Sub btnCreateUser_Click(sender As Object, e As EventArgs) Handles btnCreateUser.Click
+
+        'Query for user to make sure it doesn't already exist
+        SQL.RunQuery("SELECT * FROM Users WHERE Users.username = '" & txtUsername.Text & "' ")
+        'check row count for username
+        If SQL.SQLDataset.Tables(0).Rows.Count > 0 Then
+            MsgBox("User already exists")
+            Exit Sub
+        Else
+            CreateUser()
+            txtUsername.Clear()
+            txtPassword.Clear()
+            txtEmailAddress.Clear()
+            chkActive.Checked = True
+            chkAdmin.Checked = False
+        End If
+    End Sub
+    Public Sub CreateUser()
+        If Len(txtUsername.Text) >= 5 And Len(txtPassword.Text) >= 6 Then
+
+            'get the active/admin checkbox values
+            Dim intActive As Integer
+            Dim intAdmin As Integer
+            If chkActive.Checked = True Then intActive = 1 Else intActive = 0
+            If chkAdmin.Checked = True Then intAdmin = 1 Else intAdmin = 0
+
+            'Add user to database
+            SQL.AddUser(txtUsername.Text, txtPassword.Text, txtEmailAddress.Text, intActive, intAdmin)
+
+        Else
+            MsgBox("Username or Password is too short!")
+            Exit Sub
+        End If
+    End Sub
+
+End Class
---
+++ SQLControl.vb
@@ -0,0 +1,72 @@
+﻿Imports System.Data.Sql
+Imports System.Data.SqlClient
+
+
+
+Public Class SQLControl
+    ' Creates a new SQL Connection - requires at least 4 parameters (server pointer, database pointer, credentials"
+    Public SQLCon As New SqlConnection With {.ConnectionString = "Server=DESKTOP-VFK5HHV\SQLEXPRESSJEFF; Database=Users;User=sa;Pwd=password; "}
+    Public SQLCmd As SqlCommand 'Queries database
+    Public SQLDA As SqlDataAdapter
+    Public SQLDataset As DataSet
+
+    ''Tests that the Connection can be opened. Returns an exception if it can't
+    Public Function HasConnection() As Boolean
+        Try
+            SQLCon.Open()
+
+            SQLCon.Close()
+            Return True
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            Return False
+        End Try
+    End Function
+
+    Public Sub RunQuery(Query As String)
+        Try
+            SQLCon.Open()
+            SQLCmd = New SqlCommand(Query, SQLCon)
+
+            ' Load SQL Records for datagrid
+            'first set data adapter to reference SQL commands
+            SQLDA = New SqlDataAdapter(SQLCmd)   'creates new data adapter ref: query and connection
+            SQLDataset = New DataSet 'new instance of data set
+            SQLDA.Fill(SQLDataset) 'fill data set
+
+            SQLCon.Close()
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            'Tries closing connection if it misses the close command above.
+            If SQLCon.State = ConnectionState.Open Then
+                SQLCon.Close()
+            End If
+        End Try
+    End Sub
+
+    Public Sub AddUser(Username As String, Password As String, Email As String, IsActive As Integer, IsAdmin As Integer)
+        Try
+            Dim strInsert As String = "INSERT INTO Users (Username, Password, Email, Active, Admin)" &
+                                      "VALUES (" &
+                                      "'" & Username & "'," &
+                                      "'" & Password & "'," &
+                                      "'" & Email & "'," &
+                                      "'" & IsActive & "'," &
+                                      "'" & IsAdmin & "') "
+            SQLCon.Open()
+            SQLCmd = New SqlCommand(strInsert, SQLCon)
+
+            SQLCmd.ExecuteNonQuery()
+
+            SQLCon.Close()
+
+            MsgBox("User successfuly added")
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+
+
+End Class
